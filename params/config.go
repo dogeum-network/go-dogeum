2b95d591630920d3fd2ffe3d339567fca3544038
elixirevo <elixirevocation@gmail.com>
2023-02-16 15:34:43 +0900

    fix

--- params/config.go
+++ params/config.go
@@ -62,7 +62,7 @@ var (
 		ChainID:                       big.NewInt(6093), // dogE 6093 um
 		HomesteadBlock:                big.NewInt(0),
 		DAOForkBlock:                  big.NewInt(0),
-		DAOForkSupport:                true,
+		DAOForkSupport:                false,
 		EIP150Block:                   big.NewInt(0),
 		EIP150Hash:                    common.Hash{},
 		EIP155Block:                   big.NewInt(0),
